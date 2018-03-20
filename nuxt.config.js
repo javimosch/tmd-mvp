396fb8f9e8d5aa3c722f9f95327bf80ffa6973e1
javimosch <arancibiajav@gmail.com>
2018-03-21 00:36:03 +0100

    [UPD] yarn task fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,7 +93,7 @@ module.exports = {
         }
         let res = sander.readFileSync(p).toString('utf-8');
         res = JSON.parse(res);
-        resolve(res.list.map(i=>i.path));
+        resolve(res.nodes.map(i=>i.path));
       })
     }
   },
