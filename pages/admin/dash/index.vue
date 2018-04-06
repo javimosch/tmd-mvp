d87692cfa590090e04ea14eca1012b29d7b37964
javimosch <arancibiajav@gmail.com>
2018-04-06 19:09:17 +0200

    [WIP] backoffice dash added

---
+++ pages/admin/dash/index.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="Dashboard">
+		<h3 class="text-center">Dash</h3>
+		<div class="row justify-content-center">
+    <div class="col-2">
+      <b-btn size="lg"
+             class='w-100 mt-4'
+             to="/admin/dash"
+             variant="primary">Fields</b-btn>
+    </div>
+    <div class="col-3">
+      <b-btn size="lg"
+             class='w-100 mt-4'
+             to="/admin/dash"
+             variant="primary">Benefits</b-btn>
+    </div>
+  </div>
+	</div>
+</template>
+<script>
+	export default {
+		layout: 'app-guess',
+		name: 'Dashboard',
+		props:[],
+		fetch(){
+
+		},
+		async asyncData(){
+			return {}
+		},
+		computed:{
+
+		},
+		methods:{
+
+		},
+		components:{
+
+		},
+		created(){
+
+		},
+		mounted(){
+
+		}
+	}
+</script>
+<style lang="scss" scoped>
+.Dashboard{
+	
+}
+</style>
---
+++ pages/admin/login/index.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="AdminLogin">
+  <h3 class="text-center">Backoffice</h3>
+  <div class="row justify-content-center">
+    <div class="col-2">
+      <b-btn size="lg"
+             class='w-100 mt-4'
+             to="/admin/dash"
+             variant="primary">Login</b-btn>
+    </div>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  layout: 'app-guess',
+  name: 'AdminLogin',
+  props: [],
+  fetch() {},
+  async asyncData() {
+    return {}
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  components: {
+
+  },
+  created() {},
+  mounted() {}
+}
+
+</script>
+
+<style lang="scss" scoped="">
+.AdminLogin {}
+</style>
