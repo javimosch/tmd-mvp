de03a3e4206f51bd94ddeff0323927dbcb4b8c56
javimosch <arancibiajav@gmail.com>
2018-04-10 17:43:49 +0200

    [UPD] dashboard list improved

--- pages/admin/dash/index.vue
+++ pages/admin/dash/index.vue
@@ -1,17 +1,17 @@
 <template>
 	<div class="Dashboard">
-		<h4 class="mb-2 text-center">Mockups</h4>
+		<h4 class="mb-2 text-center">Slides for demo</h4>
 		<ul class="list-group">
-			<li class="list-group-item"><a target="_blank" href="/mockup/1">Slide 1 (1) (Landing with start button)</a></li>
-			<li class="list-group-item"><a target="_blank" href="/mockup/2">Slide 2 (3) (Start help text)</a></li>
-			<li class="list-group-item"><a target="_blank" href="/mockup/3">Slide 3 (4) (Number of benefits)</a></li>
-			<li class="list-group-item"><a target="_blank" href="/mockup/4">Slide 4 (5) (Benefits circles display)</a></li>
-			<li class="list-group-item"><a target="_blank" href="/mockup/5">Slide 5 (6) (Benefit details modal)</a></li>
-			<li class="list-group-item"><a target="_blank" href="/mockup/6">Slide 6 (7) (User reply more questions and benefits percentages increases)</a></li>
-			<li class="list-group-item"><a target="_blank" href="/mockup/7">Slide 7 (8) (Chatbot stops requesting questions, Continue button)</a></li>
-			<li class="list-group-item"><a target="_blank" href="/mockup/8">Slide 8 (9) (Order checkout)</a></li>
-			<li class="list-group-item"><a target="_blank" href="/mockup/9">Slide 9 (10) (Order checkout Information modal)</a></li>
-			<li class="list-group-item"><a target="_blank" href="/mockup/10">Slide 10 (11) (Order checkout Payment modal)</a></li>
+			<li class="list-group-item"><a target="_blank" href="/mockup/1">Slide 01 : Landing with start button.</a></li>
+			<li class="list-group-item"><a target="_blank" href="/mockup/2">Slide 02 : Start help text.</a></li>
+			<li class="list-group-item"><a target="_blank" href="/mockup/3">Slide 03 : Number of benefits.</a></li>
+			<li class="list-group-item"><a target="_blank" href="/mockup/4">Slide 04 : Benefits circles display.</a></li>
+			<li class="list-group-item"><a target="_blank" href="/mockup/5">Slide 05 : Benefit details modal.</a></li>
+			<li class="list-group-item"><a target="_blank" href="/mockup/6">Slide 06 : User reply more questions and benefits percentages increases.</a></li>
+			<li class="list-group-item"><a target="_blank" href="/mockup/7">Slide 07 : Chatbot stops requesting questions, Continue button.</a></li>
+			<li class="list-group-item"><a target="_blank" href="/mockup/8">Slide 08 : Order checkout.</a></li>
+			<li class="list-group-item"><a target="_blank" href="/mockup/9">Slide 09 : Order checkout Information modal.</a></li>
+			<li class="list-group-item"><a target="_blank" href="/mockup/10">Slide 10 : Order checkout Payment modal.</a></li>
 		</ul>
 	</div>
 </template>
