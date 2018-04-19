990d84dc43cd42964b8244cb4c36a1439fb44274
javimosch <arancibiajav@gmail.com>
2018-04-19 11:59:57 +0200

    UPD partners

--- components/tmd/landing/Partners.vue
+++ components/tmd/landing/Partners.vue
@@ -2,19 +2,24 @@
 <div class="Partners container">
   <div class="row no-gutters justify-content-center">
     <div class="col-lg-12 m-0 mt-5 mb-3 p-0">
-      <h2 class="text-center">Nos partenaires</h2>
+      <h2 class="text-center">Ils nous soutiennent</h2>
     </div>
   </div>
   <div class="row PartnersLine no-gutters justify-content-center">
-    <div class="col-12 col-md-6 col-lg-3 mt-3">
+    <div class="col-12 col-md-6 col-lg-3 mt-3 VerticalCenteredChilds">
       <img class="mx-auto d-block PartnerLogo"
-           src="~/assets/partners/logo_pantheon_sobornne.jpg" />
+           src="http://www.pariscotejardin.fr/wp-content/2012/12/logo_mairie_de_paris.jpg" />
     </div>
     
-    <div class="col-12 col-md-6 col-lg-3 mt-3">
+    <div class="col-12 col-md-6 col-lg-3 mt-3 VerticalCenteredChilds">
       <img class="mx-auto d-block PartnerLogo"
-           src="https://static.latribune.fr/590121/french-tech.jpg" />
+           src="https://ae.ambafrance.org/local/cache-vignettes/L797xH370/ae4206ec0208456b-8d487.jpg?1516012939" />
     </div>
+    <div class="col-12 col-md-6 col-lg-3 mt-3 VerticalCenteredChilds">
+      <img class="mx-auto d-block PartnerLogo"
+           src="http://www.best-masters.com/assets/img/logo_ecole/768.jpg" />
+    </div>
+    
     <div class="col-12 align-self-center mt-5" v-show="showCallToAction">
         <b-btn to="/mockup/2" class="mx-auto d-block mt-4" variant="success" size="lg">Commencer</b-btn>
       </div>
@@ -90,5 +95,10 @@ a{
 }
 a:hover{
   background: darkseagreen;
+}
+.VerticalCenteredChilds{
+   display: flex;
+    align-items: center;
+    flex-wrap: wrap;
 }
 </style>
